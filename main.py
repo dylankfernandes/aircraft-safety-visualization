2aa4bb65526d159baf851b8d2e8fd776343905f6
Dylan Fernandes <dylan.fernandes00@gmail.com>
2018-10-26 21:24:38 -0400

    finished visualization project

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
-
-style.use('fivethirtyeight')
-
-df = pd.read_csv('./safety.csv')
-print(df.head())
